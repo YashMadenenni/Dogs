d83e7ded1d6c9cdc9544823615a9697a041d48a7
Yashwanth Kumar <yash.madenenni@outlook.com>
2024-02-04 23:30:59 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="flex App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
