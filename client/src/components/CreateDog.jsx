fb4c1844f4f5bb36ba04886906ae22390eec8110
Yashwanth Kumar <yash.madenenni@outlook.com>
2024-02-09 19:25:11 +0000

    Fixed padding in create.jsx

--- client/src/components/CreateDog.jsx
+++ client/src/components/CreateDog.jsx
@@ -198,7 +198,7 @@ const CreateDog = ({ editDog, isEdit,setEditDog, handleDelete }) => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.1 }}
-        className={`  max-w-7xl m-auto relative z-0 `}
+        className={`  max-w-7xl m-auto relative z-0 sm:px-32 `}
       >
         <span className="hash-span" id="create">
           &nbsp;
